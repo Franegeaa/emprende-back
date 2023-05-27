32954831109c6df0ba5f1fdb3b6dc5b155636b80
90322 <90322@sistemas.frc.utn.edu.ar>
2023-05-27 17:25:26 -0300

    app.js con express levantado

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.send("Backend inicial tpi-backend!");
+});
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`sitio escuchando en el puerto ${port}`);
+});
