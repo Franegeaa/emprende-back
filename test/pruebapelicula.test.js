a23fea61315ce65baa3057f716d2dad02d481a10
alejo herrera <85969@sistemas.frc.utn.edu.ar>
2023-05-31 19:58:56 -0300

    api albumes lista

---
+++ test/pruebapelicula.test.js
@@ -0,0 +1,82 @@
+const request = require("supertest");
+const app = require("../index");
+const peliculaAlta = {
+  "idPelicula": 11,
+  "Titulo": "Los vengadores",
+  "Productor": "Marvel",
+  "FechaLanzamiento": "1972-06-16"
+};
+const peliculaModificacion = {
+  idPelicula: 2,
+  Titulo: "Pelicula modificada",  
+  Productor: "productor modificado",
+  FechaLanzamiento:"2023-05-18"
+};
+
+
+// metodo get
+describe("GET /api/peliculas", () => {
+  it("Deberia devolver todos las peliculas", async () => {
+    const res = await request(app).get("/api/peliculas");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          idPelicula: expect.any(Number),
+          Titulo: expect.any(String),
+          Productor: expect.any(String),
+          FechaLanzamiento: expect.any(String),
+        })
+      ]), 
+    );
+  });
+});
+
+// test route/albumes/:id GET
+describe("GET /api/peliculas/:id", () => {
+  it("Deberia devolver la peliculas con el id 1", async () => {
+    const res = await request(app).get("/api/peliculas/1");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        idPelicula: expect.any(Number),
+        Titulo: expect.any(String),
+        Productor: expect.any(String),
+        FechaLanzamiento: expect.any(String)
+      })
+    );
+  });
+});
+
+// metodo post test
+describe("POST /api/peliculas", () => {
+  it("Deberia devolver la peliculas que acabo de crear", async () => {
+    const res = await request(app).post("/api/peliculas").send(peliculaAlta);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual(
+      expect.objectContaining({
+        idPelicula: expect.any(Number),
+        Titulo: expect.any(String),
+        Productor: expect.any(String),
+        FechaLanzamiento: expect.any(String),
+      })
+    );
+  });
+});
+
+
+// metodo put test
+describe("PUT /api/peliculas/:id", () => {
+  it("Deberia devolver la pelicula con el id 2 modificado", async () => {
+    const res = await request(app).put("/api/peliculas/1").send(peliculaModificacion);
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+// test route/albumes/:id DELETE
+describe("DELETE /api/peliculas/:id", () => {
+  it("Deberia devolver la pelicula con el id 1 borrado", async () => {
+    const res = await request(app).delete("/api/peliculas/1");
+    expect(res.statusCode).toEqual(200);
+  });
+});
