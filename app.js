9ad16a4befeb5f5bc6c35b163718430c675bf034
90322 <90322@sistemas.frc.utn.edu.ar>
2023-05-31 21:40:16 -0300

    merge fran y any

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 
+require("./base-orm/sqlite-init-peliculas");  // crear base si no existe
+require("./base-orm/sqlite-init-jugadores");  // crear base si no existe
 const app = express();
 app.use(express.json()); // para poder leer json en el body
 
-app.use(express.json());
+const peliculasRouter = require("./routes/peliculas");
+app.use(peliculasRouter);
+
+const jugadoresmockRouter = require("./routes/jugadoresmock");
+app.use(jugadoresmockRouter);
+
+const jugadoresRouter = require("./routes/jugadores");
+app.use(jugadoresRouter);
+
 
 app.get("/", (req, res) => {
   res.send("Backend inicial tpi-backend!");
