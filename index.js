c29524ff6ff86b7803a5e103dc8dd10a8b0c6cfa
90322 <90322@sistemas.frc.utn.edu.ar>
2023-06-17 16:41:37 -0300

    cors y puerto 4000

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const cors = require('cors');
 require("./base-orm/sqlite-init-peliculas");  // crear base si no existe
 require("./base-orm/sqlite-init-jugadores");  // crear base si no existe
 const app = express();
@@ -12,7 +12,7 @@ app.use(peliculasRouter);
 
 const jugadoresRouter = require("./routes/jugadores");
 app.use(jugadoresRouter);
-
+app.use(cors())
 
 const autos = require("./routes/autos");
 app.use(autos);
@@ -28,7 +28,7 @@ app.use(albumesRouter);
 
 // levantar servidor
 if (!module.parent) {   // si no es llamado por otro modulo, es decir, si es el modulo principal -> levantamos el servidor
-  const port = process.env.PORT || 3000;   // en produccion se usa el puerto de la variable de entorno PORT
+  const port = process.env.PORT || 4000;   // en produccion se usa el puerto de la variable de entorno PORT
   app.locals.fechaInicio = new Date();
   app.listen(port, () => {
     console.log(`sitio escuchando en el puerto ${port}`);
